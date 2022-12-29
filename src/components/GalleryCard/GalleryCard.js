9efa8154824fe3a799655a32340bee2e49974750
Farid Ahmed <faridahmed.tech@gmail.com>
2022-12-29 22:53:33 +0530

    added image component

--- src/components/GalleryCard/GalleryCard.js
+++ src/components/GalleryCard/GalleryCard.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+import styles from "./GalleryCard.module.css";
 
 function GalleryCard(){
+    const [imageList, setImageList] = useState([
+        {name: <img className={styles.imageCard} src="" alt =""/> },
+        {name: <img className={styles.imageCard} src="" alt =""/> },
+        {name: <img className={styles.imageCard} src="" alt =""/> },
+        {name: <img className={styles.imageCard} src="" alt =""/> }
+    ]);
     return (
-        <div>GalleryCard</div>
+        <div className={styles.imageGrid}>
+            {imageList.map((image) => (
+                <div>
+                    {image.name}
+                </div>
+            ))}
+                
+        </div>
     )
 
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,9 +10,11 @@ import styles from "./Home.module.css";
 function Home(){
     return (
         <div className={styles.container}>
-        <Category />
-        <GalleryCard />
-        <Button />
+            <div className={styles.innerContainer}>
+                <Category />
+                <GalleryCard />
+            </div>
+            <Button />
         </div>
        
     );
